5ef62b4a48596ee8025282ea839002ad6d31e7b0
Aman <amansahu47388@gmail.com>
2026-03-27 10:32:39 +0530

    update

--- Backend/build.sh
+++ Backend/build.sh
@@ -5,5 +5,5 @@ pip install -r requirements.txt
 python manage.py collectstatic --noinput
 
 python manage.py migrate
-
+python manage.py migrate --fake-initial
 gunicorn Hospital_Project.asgi:application --bind
