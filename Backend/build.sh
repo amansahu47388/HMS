964a8816d48eb83ac3abe69e7639021f14769e90
Aman <amansahu47388@gmail.com>
2026-03-27 11:29:25 +0530

    update build

--- Backend/build.sh
+++ Backend/build.sh
@@ -6,4 +6,4 @@ python manage.py collectstatic --noinput
 
 python manage.py migrate
 python manage.py migrate --fake-initial
-gunicorn Hospital_Project.asgi:application --bind
+gunicorn Hospital_Project.asgi:application --bind 0.0.0.0:$PORT
