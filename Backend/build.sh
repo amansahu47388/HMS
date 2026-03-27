80ce811c01b6bdd3268882b050bd7e39c6fa812c
Aman <amansahu47388@gmail.com>
2026-03-27 14:59:28 +0530

    update build

--- Backend/build.sh
+++ Backend/build.sh
@@ -5,3 +5,7 @@ pip install -r requirements.txt
 python manage.py collectstatic --noinput
 
 python manage.py migrate --noinput --fake-initial
+
+if [[$CREATE_SUPERUSER]]; then
+    python manage.py createsuperuser --noinput
+fi
