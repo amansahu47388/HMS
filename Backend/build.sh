85aa6cade300c2f2fbbf02cf17d97dec3292d27e
Aman <amansahu47388@gmail.com>
2026-03-28 19:12:46 +0530

    update build

--- Backend/build.sh
+++ Backend/build.sh
@@ -6,6 +6,8 @@ python manage.py collectstatic --noinput
 
 python manage.py migrate --noinput --fake-initial
 
-if [[$CREATE_SUPERUSER]]; then
+if [ "$CREATE_SUPERUSER" = "true" ]; then
     python manage.py createsuperuser --noinput
 fi
+
+# python scripts/ensure_superuser.py
